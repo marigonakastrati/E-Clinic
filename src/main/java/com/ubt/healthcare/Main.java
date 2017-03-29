b633ddec1b0e72edca7ed9dcb9adcb4beebe6a18
Fatlonder <fatlondercakolli@gmail.com>
2017-03-29 14:20:02 +0200

    Naming Standards

---
+++ src/main/java/com/ubt/healthcare/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ubt.healthcare;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ *
+ * @author F
+ */
+@Path("login")
+public class Main {
+    
+    @GET
+    @Path("/msg")
+    public String msg()
+    {
+        return "Message";
+    }
+}
